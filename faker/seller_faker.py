8fbcbf1d05ac73dd3e77fff000eddcc3422b3cc1
mariaeduardatc <81043486+mariaeduardatc@users.noreply.github.com>
2023-04-07 15:29:24 +0530

    sellers faker, fixes

---
+++ faker/seller_faker.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker 
+from sqlalchemy.ext.declarative import declarative_base
+
+from databases.seller_db import Seller
+
+
+from faker import Faker
+
+def create_seller(holder):
+    for i in range(holder):
+        fake = Faker()
+        sellers = Seller(**{
+            "name": fake.name(),
+            "email": fake.email()
+            })
+        
+        # to create the db
+        engine = create_engine('sqlite:///seller.db')
+        engine.connect()
+
+        Base = declarative_base() 
+
+        Base.metadata.create_all(bind=engine) 
+
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        session.add(sellers)
+        session.commit()
