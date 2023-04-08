b1aa9a3e38128646dc89350917d3ff6d0256aedd
mariaeduardatc <81043486+mariaeduardatc@users.noreply.github.com>
2023-04-08 15:21:12 +0530

    fixes, comission calc

--- databases/comissions_db.py
+++ databases/comissions_db.py
@@ -1,9 +1,14 @@
-from sqlalchemy import Column, Float, ForeignKey, Text, Integer
+from sqlalchemy import Column, Float, ForeignKey, Integer, create_engine
+from sqlalchemy.orm import sessionmaker 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
 from databases.agent_db import Agent
 
+	
+# to create the db
+engine = create_engine('sqlite:///agent.db')
+engine.connect()  
 
 Base = declarative_base() 
 
@@ -18,3 +23,8 @@ class Comissions(Base):
 	
 	def __repr__(self):
 		return "<Comissions(id={0}, agent_id={1}, comission={2})>".format(self.id, self.agent_id, self.comission)
+
+Base.metadata.create_all(bind=engine) 
+
+Session = sessionmaker(bind=engine)
+session = Session()
--- insert_data.py
+++ insert_data.py
@@ -1,3 +1,6 @@
+from requests import session
+from databases.comissions_db import Comissions
+
 from faker.agent_faker import create_agent
 from faker.buyer_faker import create_buyer
 from faker.house_faker import create_house
@@ -21,3 +24,24 @@ create_house(house_num, agents_num)
 create_office(office_num)
 create_sales(sales_num, house_num, buyers_num, agents_num)
 create_seller(seller_num)
+
+# populating the comissions' db
+for sale in sales_num:
+    id_agent = session.query(sale).filter_by(id=sale.id).first().agent_id
+    sale_price = session.query(sale).filter_by(id=sale.id).first().price
+
+    if sale_price <= 100000:
+        comission = sale_price*0.1
+    elif 100000 < sale_price <= 200000:
+        comission = sale_price*0.075
+    elif 200000 < sale_price <= 500000:
+        comission = sale_price*0.06
+    elif 500000 < sale_price <= 1000000:
+        comission = sale_price*0.05
+    else:
+        comission = sale_price*0.04
+    
+    comission_db = Comissions(agent_id = id_agent, comission = comission)
+    
+    session.add(comission_db)
+    session.commit()
